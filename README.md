4669f55ee9580315cf71ed45320d1dd18262b469
Jeff Capuano <jcapuano328@gmail.com>
2015-12-06 08:57:18 -0500

    nspec: it works, but how to integrate into vs?

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # aps.mvc
 
-ASP.NET MVC4, Castle Windsor, WebAPI on backend.
+ASP.NET MVC5, Castle Windsor, WebAPI on backend.
 
 AngularJS on frontend.
 
+### Back end
+https://github.com/castleproject/Windsor
 
 https://github.com/StackExchange/dapper-dot-net
+
+### Front end
 https://github.com/angular-ui/ui-grid
+
 http://ui-grid.info/docs/#/tutorial/201_editable
+
 http://ui-grid.info/docs/#/api/ui.grid.class:GridOptions
+
 http://ui-grid.info/docs/#/api/ui.grid.class:GridApi
+
 http://ui-grid.info/docs/#/api/ui.grid.class:GridOptions.columnDef
+
+### Testing
+https://xunit.github.io/
+
+http://nsubstitute.github.io/
+
+http://docs.shouldly-lib.net/v2.4.0/docs
+
+http://nspec.org/
+
+http://codeforcoffee.org/setting-up-angular-js-jasmine-and-karma-in-visual-studio/
---
+++ tests/core/DateTimeExSpec.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NSpec;
+using Shouldly;
+
+using ica.aps.core;
+
+namespace Core
+{    
+    class DateTimeEx_Spec : nspec
+    {
+        DateTime _now;
+        void before_each()
+        {
+            _now = DateTime.Now;
+        }
+
+        void work_week()
+        {
+            WorkWeek ww = null;
+            beforeEach = () => ww = _now.WorkWeek();
+
+            it["should not be null"] = () => {
+                ww.ShouldNotBe(null);
+            };
+
+            it["should start on monday"] = () => {
+                ww.Start.DayOfWeek.ShouldBe(System.DayOfWeek.Monday);
+            };
+
+            it["should end on saturday"] = () => {
+                ww.End.DayOfWeek.ShouldBe(System.DayOfWeek.Saturday);
+            };                       
+        }
+    }
+}
